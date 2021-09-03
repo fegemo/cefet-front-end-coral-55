2f190d5d8d7df0b0e070477cbd7d96a3d4c74b53
Flavio Coutinho <fegemo@gmail.com>
2021-09-03 14:05:20 -0300

    Fixes bubbles not cleaning up
    In some circunstances, they were born already offscreen
    to the top and never triggered transitionend.
    
    I changed the use of a js injected css transition
    to web animation api (doesnt work on ie, but who cares)

--- js/bubbles.js
+++ js/bubbles.js
@@ -3,9 +3,9 @@ const TEMPO_VARIAVEL = 3000;  // 3s
 const bolhaEl = carregaUmaImagem('images/bolha.png');
 
 function carregaUmaImagem(path) {
-    const img = new Image();
-    img.src = path;
-    return img;
+  const img = new Image();
+  img.src = path;
+  return img;
 }
 
 function getLarguraJanela() {
@@ -19,43 +19,38 @@ function getLarguraJanela() {
 }
 
 
-function criaUmaBolha() {
-    const novaBolhaEl = bolhaEl.cloneNode();
-    const posicaoX = Math.random() * getLarguraJanela() + 'px';
-    // Math.random() retorna um número aleatório de 0 até 1
-    // ... daí multiplicamos esse número pela largura da janela
+async function criaUmaBolha() {
+  const novaBolhaEl = bolhaEl.cloneNode();
+  const posicaoX = Math.random() * getLarguraJanela() + 'px';
+  // Math.random() retorna um número aleatório de 0 até 1
+  // ... daí multiplicamos esse número pela largura da janela
 
-    novaBolhaEl.style.position = 'fixed';
-    novaBolhaEl.style.transition = 'all 5s linear';
-    novaBolhaEl.style.bottom = '-50px';
-    novaBolhaEl.style.left = posicaoX;
-    novaBolhaEl.style.opacity = 1;
-    novaBolhaEl.style.transform = 'scale(' + (Math.random() / 2 + 0.5) + ')';
+  novaBolhaEl.style.position = 'fixed';
+  novaBolhaEl.style.transition = 'all 5s linear';
+  novaBolhaEl.style.bottom = '-50px';
+  novaBolhaEl.style.left = posicaoX;
+  novaBolhaEl.style.opacity = 1;
+  novaBolhaEl.style.transform = `scale(${Math.random() / 2 + 0.5})`;
 
-    document.body.appendChild(novaBolhaEl);
+  document.body.appendChild(novaBolhaEl);
+  const animation = novaBolhaEl.animate([
+    { bottom: '-50px' },
+    { bottom: window.innerHeight + 'px' }
+  ], {
+    duration: 5000,
+    iterations: 1
+  });
 
-    window.setTimeout(function() {
-        novaBolhaEl.style.opacity = .5;
-        novaBolhaEl.style.bottom = window.innerHeight + 'px';
-    }, 0);
-
-    // remove o elemento da imagem da bolha assim que sua transição terminar
-    (function(oldbolhaEl) {
-      function removeElement() {
-        oldbolhaEl.removeEventListener('transitionend', removeElement);
-        document.body.removeChild(oldbolhaEl);
-      }
-      oldbolhaEl.addEventListener('transitionend', removeElement);
-    }(novaBolhaEl));
+  // pelo menos, vai esperar por TEMPO_MINIMO. Mas pode, adicionalmente,
+  // esperar por mais [0%....100%] x TEMPO_VARIAVEL
+  const proximaBolhaDaqui = TEMPO_MINIMO + Math.random() * TEMPO_VARIAVEL;
+  window.setTimeout(criaUmaBolha, proximaBolhaDaqui);
 
 
-    // pelo menos, vai esperar por TEMPO_MINIMO. Mas pode, adicionalmente,
-    // esperar por mais [0%....100%] x TEMPO_VARIAVEL
-    const proximaBolhaDaqui = TEMPO_MINIMO + Math.random()*TEMPO_VARIAVEL;
-    window.setTimeout(criaUmaBolha, proximaBolhaDaqui);
+  // remove o elemento da imagem da bolha assim que sua transição terminar
+  await animation.finished
+  document.body.removeChild(novaBolhaEl);
 }
-
-
 
 
 criaUmaBolha();
